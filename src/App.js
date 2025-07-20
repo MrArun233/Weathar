c9c2826f57ace0ae8ba7420f3b6f9640b9f54cfa
techie777 <techsculptor8@gmail.com>
2025-07-20 19:57:40 +0530

    Fix: removed unused imports and resolved ESLint warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import FormPost from './TaskApp/FormPost'
 import './App.css'
-import FormGet from './TaskApp/FormGet'
-import FormUpdate from './TaskApp/FormUpdate'
 const App = () => {
   return (
    <div>
--- src/TaskApp/FormGet.js
+++ src/TaskApp/FormGet.js
@@ -1,21 +1,22 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import { DataContext } from "./Context/DataContext";
 import FormUpdate from "./FormUpdate";
 
 const FormGet = () => {
-  const {inputdata,FormGetFetch} = useContext(DataContext);
+  const { inputdata, FormGetFetch } = useContext(DataContext);
   const safeArray = Array.isArray(inputdata) ? inputdata : [];
-useEffect(()=>{
-FormGetFetch()
-console.log('props.FormGetFetch() function call')
-},[])
+
+  useEffect(() => {
+    FormGetFetch();
+    console.log("props.FormGetFetch() function call");
+  }, [FormGetFetch]);
 
   return (
     <div className="get-men-div">
       <ul>
-      {safeArray.map((item,i)=>(
-        <FormUpdate key={i} id={item.id} showData={item.inputValue}/>
-      ))}
+        {safeArray.map((item, i) => (
+          <FormUpdate key={i} id={item.id} showData={item.inputValue} />
+        ))}
       </ul>
     </div>
   );
